749805bee4c42d76090c37bb47d6ba2c06cddbc4
Kasia Krejszeff <kasiakrejszeff@gmail.com>
2015-06-08 18:41:15 +0200

    Fix bug in automat.py

--- komisje/management/commands/automat.py
+++ komisje/management/commands/automat.py
@@ -4,48 +4,50 @@ import urllib
 from bs4 import BeautifulSoup
 import sys
 
+
+wyb_url = "http://prezydent2010.pkw.gov.pl/PZT/PL/WYN/W/index.htm"
+base_url = wyb_url.rsplit('/',1)[0]
+
+def open_conn(url):
+    filehandle = urllib.urlopen(url)
+    html_doc = filehandle.read()
+    soup = BeautifulSoup(html_doc)
+    soup = soup.find(id="s0")
+    soup_tbody = soup.find_all("tbody")
+    soup = soup_tbody[0]
+    soup_tr = soup.find_all("tr")
+    return soup_tr
+
+
+def parse_page(url):
+    soup_tr = open_conn(url)
+    tab =[]
+    for tr in soup_tr:
+        a = tr.find("a")
+        tab.append([a.string, base_url + "/" + a["href"]])  
+    return tab
+
+def parse_zagr_page(url, pm):
+    soup_tr = open_conn(url)
+    okr = []
+    for tr in soup_tr:
+        a = tr.find("a")
+        if not a:
+            a = tr.find("td", { "class":"col2al" })
+            a = a.string[2:]
+            gm = Gmina(name=a, powiat=pm)
+            gm.save()    
+            for o in okr:
+                om = Okreg(name=o, gmina=gm, karty=0, wyborcy=0)
+                om.save()
+            okr[:] = []
+        else:
+            okr.append(a.string)  
 class Command(BaseCommand):
 
     help = "Populates db with data"
 
-    wyb_url = "http://prezydent2010.pkw.gov.pl/PZT/PL/WYN/W/index.htm"
-    base_url = wyb_url.rsplit('/',1)[0]
 
-    def open_conn(url):
-        filehandle = urllib.urlopen(url)
-        html_doc = filehandle.read()
-        soup = BeautifulSoup(html_doc)
-        soup = soup.find(id="s0")
-        soup_tbody = soup.find_all("tbody")
-        soup = soup_tbody[0]
-        soup_tr = soup.find_all("tr")
-        return soup_tr
-
-
-    def parse_page(url):
-        soup_tr = open_conn(url)
-        tab =[]
-        for tr in soup_tr:
-            a = tr.find("a")
-            tab.append([a.string, base_url + "/" + a["href"]])  
-        return tab
-
-    def parse_zagr_page(url, pm):
-        soup_tr = open_conn(url)
-        okr = []
-        for tr in soup_tr:
-            a = tr.find("a")
-            if not a:
-                a = tr.find("td", { "class":"col2al" })
-                a = a.string[2:]
-                gm = Gmina(name=a, powiat=pm)
-                gm.save()    
-                for o in okr:
-                    om = Okreg(name=o, gmina=gm, karty=0, wyborcy=0)
-                    om.save()
-                okr[:] = []
-            else:
-                okr.append(a.string)  
 
 
     def handle(self, **options):
